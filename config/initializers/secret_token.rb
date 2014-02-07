e0e8852806459af517d64405716c14099c384d1b
pramodshinde <pramod@joshsoftware.com>
2014-02-07 18:33:43 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4DeviseSocialLogins::Application.config.secret_key_base = '0d1ac4f2c0afcb1d3ff5784504162f1098a2077fb4be6328a4932d69de9c896dcf49734388792790038f3afc21c635e7944920e95e1d7827fef70f7e6df11e7f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails4DeviseSocialLogins::Application.config.session_store :cookie_store, key: '_rails4_devise_social_logins_session'
